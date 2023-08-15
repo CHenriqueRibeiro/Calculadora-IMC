0b095321e840f88030280757d9572925b68db516
Carlos Henrique <124113375+CHenriqueRibeiro@users.noreply.github.com>
2023-08-15 13:13:44 -0300

    falta ajustar placehorlder

--- index.js
+++ index.js
@@ -1,60 +1,66 @@
-document.getElementById('btncalculo').addEventListener('click', calcular)
+$(document).ready(() => {
+    $('#pesodousuario').mask('999 Kg')
+    $('#alturadousuario').mask("0,00");
+    $('#btncalculo').on('click', calcular)
+})
+
 
 
 function calcular() {
-    let name = document.getElementById('nomedousuario').value
-    let idade = document.getElementById('idadedousuario').value
-    let peso = document.getElementById('pesodousuario').value
-    let altura = document.getElementById('alturadousuario').value
-    let display = document.getElementById('parteinferior').setAttribute('id', 'parteinferior-on')
+    let name = $('#nomedousuario').val()
+    let idade = $('#idadedousuario').val()
+    let peso = $('#pesodousuario').val()
+    let altura = $('#alturadousuario').val().replace(',', '.')
+    let display = $('#parteinferior')
+    let genero = document.querySelector('input[name="genero"]:checked').value
     let resAltura = altura * altura
     let imc = peso / resAltura
-
+    display.addClass('parteinferior-on')
 
     if (imc < 18.5) {
-        console.log(display)
-        console.log(document.querySelector('li.nomepessoa').innerText = `Nome: ${name} `)
-        console.log(document.querySelector('li.idadepessoa').innerText = `Idade: ${idade} anos `)
-        console.log(document.querySelector('li.generopessoa').innerText = `Genero: ${genero} `)
-        console.log(document.querySelector('li.pesopessoa').innerText = `Peso: ${peso} kg `)
-        console.log(document.querySelector('li.alturapessoa').innerText = `Altura: ${altura} cm `)
-        console.log(document.querySelector('p.valorimc').innerText = `Seu IMC é: ${imc.toFixed(2)} `)
-        console.log(document.querySelector('p.classificacaoimc').innerText = `Sua classificação é de : Magreza `)
+
+        console.log($('li.nomepessoa').text(`Nome: ${name} `))
+        console.log($('li.idadepessoa').text(`Idade: ${idade} anos `))
+        console.log($('li.generopessoa').text(`Genero: ${genero} `))
+        console.log($('li.pesopessoa').text(`Peso: ${peso} kg `))
+        console.log($('li.alturapessoa').text(`Altura: ${altura} cm `))
+        console.log($('p.valorimc').text(`Seu IMC é: ${imc.toFixed(2)} `))
+        console.log($('p.classificacaoimc').text(`Sua classificação é de : Magreza`))
     } else if (imc >= 18.5 && imc < 25) {
-        console.log(document.querySelector('li.nomepessoa').innerText = `Nome: ${name} `)
-        console.log(document.querySelector('li.idadepessoa').innerText = `Idade: ${idade} anos `)
-        console.log(document.querySelector('li.generopessoa').innerText = `Genero: ${genero} `)
-        console.log(document.querySelector('li.pesopessoa').innerText = `Peso: ${peso} kg `)
-        console.log(document.querySelector('li.alturapessoa').innerText = `Altura: ${altura} cm `)
-        console.log(document.querySelector('p.valorimc').innerText = `Seu IMC é: ${imc.toFixed(2)} `)
-        console.log(document.querySelector('p.classificacaoimc').innerText = `Sua classificação é de : Peso Normal`)
+        console.log($('li.nomepessoa').text(`Nome: ${name} `))
+        console.log($('li.idadepessoa').text(`Idade: ${idade} anos `))
+        console.log($('li.generopessoa').text(`Genero: ${genero} `))
+        console.log($('li.pesopessoa').text(`Peso: ${peso} kg `))
+        console.log($('li.alturapessoa').text(`Altura: ${altura} cm `))
+        console.log($('p.valorimc').text(`Seu IMC é: ${imc.toFixed(2)} `))
+        console.log($('p.classificacaoimc').text(`Sua classificação é de : Peso Normal`))
     } else if (imc >= 25 && imc < 30) {
-        console.log(document.querySelector('li.nomepessoa').innerText = `Nome: ${name} `)
-        console.log(document.querySelector('li.idadepessoa').innerText = `Idade: ${idade} anos `)
-        console.log(document.querySelector('li.generopessoa').innerText = `Genero: ${genero} `)
-        console.log(document.querySelector('li.pesopessoa').innerText = `Peso: ${peso} kg `)
-        console.log(document.querySelector('li.alturapessoa').innerText = `Altura: ${altura} cm `)
-        console.log(document.querySelector('p.valorimc').innerText = `Seu IMC é: ${imc.toFixed(2)} `)
-        console.log(document.querySelector('p.classificacaoimc').innerText = `Sua classificação é de : Sobrepeso`)
+        console.log($('li.nomepessoa').text(`Nome: ${name} `))
+        console.log($('li.idadepessoa').text(`Idade: ${idade} anos `))
+        console.log($('li.generopessoa').text(`Genero: ${genero} `))
+        console.log($('li.pesopessoa').text(`Peso: ${peso} kg `))
+        console.log($('li.alturapessoa').text(`Altura: ${altura} cm `))
+        console.log($('p.valorimc').text(`Seu IMC é: ${imc.toFixed(2)} `))
+        console.log($('p.classificacaoimc').text(`Sua classificação é de : Sobrepeso`))
     } else if (imc >= 30 && imc < 40) {
-        console.log(document.querySelector('li.nomepessoa').innerText = `Nome: ${name} `)
-        console.log(document.querySelector('li.idadepessoa').innerText = `Idade: ${idade} anos `)
-        console.log(document.querySelector('li.generopessoa').innerText = `Genero: ${genero} `)
-        console.log(document.querySelector('li.pesopessoa').innerText = `Peso: ${peso} kg `)
-        console.log(document.querySelector('li.alturapessoa').innerText = `Altura: ${altura} cm `)
-        console.log(document.querySelector('p.valorimc').innerText = `Seu IMC é: ${imc.toFixed(2)} `)
-        console.log(document.querySelector('p.classificacaoimc').innerText = `Sua classificação é de : Obesidade `)
+        console.log($('li.nomepessoa').text(`Nome: ${name} `))
+        console.log($('li.idadepessoa').text(`Idade: ${idade} anos `))
+        console.log($('li.generopessoa').text(`Genero: ${genero} `))
+        console.log($('li.pesopessoa').text(`Peso: ${peso} kg `))
+        console.log($('li.alturapessoa').text(`Altura: ${altura} cm `))
+        console.log($('p.valorimc').text(`Seu IMC é: ${imc.toFixed(2)} `))
+        console.log($('p.classificacaoimc').text(`Sua classificação é de : Obesidade`))
     } else if (imc > 40) {
-        console.log(document.querySelector('li.nomepessoa').innerText = `Nome: ${name} `)
-        console.log(document.querySelector('li.idadepessoa').innerText = `Idade: ${idade} anos `)
-        console.log(document.querySelector('li.generopessoa').innerText = `Genero: ${genero} `)
-        console.log(document.querySelector('li.pesopessoa').innerText = `Peso: ${peso} kg `)
-        console.log(document.querySelector('li.alturapessoa').innerText = `Altura: ${altura} cm `)
-        console.log(document.querySelector('p.valorimc').innerText = `Seu IMC é: ${imc.toFixed(2)} `)
-        console.log(document.querySelector('p.classificacaoimc').innerText = `Sua classificação é de : Obsesidade Grave `)
+        console.log($('li.nomepessoa').text(`Nome: ${name} `))
+        console.log($('li.idadepessoa').text(`Idade: ${idade} anos `))
+        console.log($('li.generopessoa').text(`Genero: ${genero} `))
+        console.log($('li.pesopessoa').text(`Peso: ${peso} kg `))
+        console.log($('li.alturapessoa').text(`Altura: ${altura} cm `))
+        console.log($('p.valorimc').text(`Seu IMC é: ${imc.toFixed(2)} `))
+        console.log($('p.classificacaoimc').text(`Sua classificação é de : Obsesidade Grave`))
     }
 
-
+    $('input[type="text"],input[type="number"]').val('')
 
 }
 
